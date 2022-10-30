c90dcef54368a14e6f1ea1a52ecb0decf5c1bf23
Noah Emke <noahremke@gmail.com>
2022-10-30 03:34:19 -0400

    First attempt at a get save data method

---
+++ devcade-library/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devcade
+{
+  public static class Data
+  {
+    public static async Task<String> GetSaveData(String s3Object)
+    {
+      Console.Out.WriteLine("GetData|" + s3Object);
+      while (await Console.In.ReadLineAsync() != "<DATA>") { }
+
+      string data = "";
+      string line;
+      do
+      {
+        line = await Console.In.ReadLineAsync();
+        if (line != "<EOM>")
+        {
+          data += line;
+        }
+      } while (line != "<EOM>");
+
+      return data;
+    }
+  }
+}
